a5c8e256c2d529b5ae6155670eda1d068c157df8
Joost Soeterbroek <j.soeterbroek@dictu.nl>
2025-02-09 23:51:46 +0100

    add dashboard pic

--- dashboard/dashboard.ino
+++ dashboard/dashboard.ino
@@ -45,10 +45,44 @@ void setup(void)
 
 
 void drawscreen() {
-  gfx->drawRect(8, 8, 60, 40, RGB565_BLACK);
-  gfx->drawRect(8, 47, 60, 40, RGB565_BLACK);
-  gfx->drawRect(8, 86, 60, 40, RGB565_BLACK);
-  gfx->drawRect(8, 125, 60, 40, RGB565_BLACK);
+  gfx->setFont(&FreeMono8pt7b);
+  gfx->drawRect(6, 6, 308, 230, LIGHTGREY);
+
+  // 1 speed 
+  gfx->drawRect(12, 22, 102, 100, LIGHTGREY);
+
+  // 2 RPM
+  gfx->drawRect(113, 22, 102, 100, LIGHTGREY);
+
+  // logo
+  gfx->setCursor(44, 150);
+  gfx->setFont(&FreeSansBold10pt7b);
+  gfx->println("// BMW R1100 GS");  
+
+  // 3 left indic
+  gfx->drawRect(13, 164, 34, 60, LIGHTGREY);
+
+  // 4 batt warn
+  gfx->drawRect(46, 164, 34, 30, LIGHTGREY);
+
+  // 5 high beam 
+  gfx->drawRect(46, 193, 34, 31, LIGHTGREY);
+
+  // 6 fuel warn
+  gfx->drawRect(79, 164, 34, 60, LIGHTGREY);
+
+  // 7 neutral warn
+  gfx->drawRect(112, 164, 34, 60, LIGHTGREY);
+
+  // 8 oil warn
+  gfx->drawRect(145, 164, 34, 30, LIGHTGREY);
+
+  // 9 free (ABS)
+  gfx->drawRect(145, 193, 34, 31, LIGHTGREY);
+
+  // 10 right indic
+  gfx->drawRect(178, 164, 34, 60, LIGHTGREY);
+
 }
 
 void drawdata() {
@@ -56,40 +90,56 @@ void drawdata() {
   gfx->setTextColor(RGB565_BLACK);
 
   /*****************************************************************************
-   * Speed
+   * - 1 - Speed
    ****************************************************************************/
-  gfx->setCursor(14, 20);
+  // gfx->setCursor(14, 20);
+  // gfx->setFont(&FreeMono8pt7b);
+  // gfx->println("Speed");
+  // gfx->setCursor(14, 40);
+  // gfx->setFont(&FreeSansBold10pt7b);
+  // gfx->println("  70");  
+  gfx->setCursor(40, 114);
   gfx->setFont(&FreeMono8pt7b);
-  gfx->println("Speed");
-  gfx->setCursor(14, 40);
-  gfx->setFont(&FreeSansBold10pt7b);
-  gfx->println("  70");  
+  gfx->println("KM/h");
 
   /*****************************************************************************
-   * RPM 
+   * - 2 - RPM 
    ****************************************************************************/
-  gfx->setCursor(14, 60);
+  // gfx->setCursor(14, 80);
+  // gfx->setFont(&FreeSansBold10pt7b);
+  // gfx->println("  33");  
+  gfx->setCursor(148, 114);
   gfx->setFont(&FreeMono8pt7b);
-  gfx->println(" RPM");
-  gfx->setCursor(14, 80);
-  gfx->setFont(&FreeSansBold10pt7b);
-  gfx->println("  33");  
+  gfx->println("RPM");
 
   /*****************************************************************************
-   * FUEL 
+   * - 6 - FUEL 
    ****************************************************************************/
   //gfx->drawRect(8, 86, 60, 40, RGB565_BLACK);
-  gfx->fillRect(9, 87, 58, 38, RGB565_ORANGE);
-  gfx->setCursor(14, 100);
-  gfx->setFont(&FreeMono8pt7b);
-  gfx->println("FUEL");
-  gfx->setCursor(14, 120);
-  gfx->setFont(&FreeSansBold10pt7b);
-  gfx->println(" 13%");  
+  // gfx->fillRect(9, 87, 58, 38, RGB565_ORANGE);
+  // gfx->setCursor(14, 100);
+  // gfx->setFont(&FreeMono8pt7b);
+  // gfx->println("FUEL");
+  // gfx->setCursor(14, 120);
+  // gfx->setFont(&FreeSansBold10pt7b);
+  // gfx->println(" 13%");  
+
+  // demo fuel warn
+  gfx->fillRect(80, 165, 32, 58, ORANGE);
+
+  /*****************************************************************************
+   * - 10 - RIGHT INDICATOR 
+   ****************************************************************************/  
+
+  // demo right indic flashing
+  gfx->fillRect(179, 165, 32, 58, GREEN);
+  delay(1000); // 1 second
+  gfx->fillRect(179, 165, 32, 58, WHITE);
+
 }
 
 void loop() {
   drawscreen();
   drawdata();
-  delay(2000); // 2 second
+  delay(500); // 1/2 second
 }
