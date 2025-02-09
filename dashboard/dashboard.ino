b872153b3ccce7e4f13150913342c9b4e2426ecf
Joost Soeterbroek <j.soeterbroek@dictu.nl>
2025-02-09 20:11:52 +0100

    working version

--- dashboard/dashboard.ino
+++ dashboard/dashboard.ino
@@ -1,8 +1,14 @@
 /*******************************************************************************
  ******************************************************************************/
 #include <Arduino_GFX_Library.h>
-#include <NotoSansBold36.h>
+// #include <NotoSansBold15.h>
+#include <FreeMono8pt7b.h>
+#include <FreeSansBold10pt7b.h>
+#include <FreeSerifBoldItalic12pt7b.h>
 
+/*******************************************************************************
+ * Start of Arduino_GFX setting
+ ******************************************************************************/
 #define GFX_BL DF_GFX_BL // default backlight pin, you may replace DF_GFX_BL to actual backlight pin
 #define TFT_BL 27
 /* More dev device declaration: https://github.com/moononournation/Arduino_GFX/wiki/Dev-Device-Declaration */
@@ -10,12 +16,7 @@
 Arduino_GFX *gfx = create_default_Arduino_GFX();
 #else /* !defined(DISPLAY_DEV_KIT) */
 
-/* More data bus class: https://github.com/moononournation/Arduino_GFX/wiki/Data-Bus-Class */
-//Arduino_DataBus *bus = create_default_Arduino_DataBus();
-
 /* More display class: https://github.com/moononournation/Arduino_GFX/wiki/Display-Class */
-//Arduino_GFX *gfx = new Arduino_ILI9341(bus, DF_GFX_RST, 0 /* rotation */, false /* IPS */);
-
 Arduino_DataBus *bus = new Arduino_ESP32SPI(2 /* DC */, 15 /* CS */, 14 /* SCK */, 13 /* MOSI */, GFX_NOT_DEFINED /* MISO */);
 Arduino_GFX *gfx = new Arduino_ST7789(bus, -1 /* RST */, 1 /* rotation */, true /* IPS */);
 
@@ -23,6 +24,7 @@ Arduino_GFX *gfx = new Arduino_ST7789(bus, -1 /* RST */, 1 /* rotation */, true 
 /*******************************************************************************
  * End of Arduino_GFX setting
  ******************************************************************************/
+
 
 void setup(void)
 {
@@ -32,29 +34,26 @@ void setup(void)
 #ifdef TFT_BL
     pinMode(TFT_BL, OUTPUT);
     digitalWrite(TFT_BL, HIGH);
-
-    // ledcSetup(0, 2000, 8);
-    // ledcAttachPin(TFT_BL, 0);
-    // ledcSetup(LEDC_CHANNEL, 50, LEDC_RESOLUTION); // Set frequency to 50Hz, resolution to 10 bits
-    // ledcAttachPin(LEDC_PIN, LEDC_CHANNEL); // Associate GPIO pin with LEDC channel
     ledcAttach(0, 2000, 8);
     ledcWrite(0, 255); /* Screen brightness can be modified by adjusting this parameter. (0-255) */
-
 #endif
 
-    gfx->setCursor(10, 10);
-    gfx->setTextColor(RED);
-    gfx->println("Hello World!");
-
+    // gfx->setCursor(10, 10);
+    // gfx->setTextColor(RED);
+    // gfx->println("Setup complete...");
     delay(5000); // 5 seconds
 }
 
 void loop()
 {
-    gfx->setCursor(random(gfx->width()), random(gfx->height()));
-    gfx->setTextColor(random(0xffff), random(0xffff));
-    gfx->setTextSize(random(6) /* x scale */, random(6) /* y scale */, random(2) /* pixel_margin */);
-    gfx->println("Hello World!");
-
+    gfx->setCursor(20,20);
+    gfx->setTextColor(BLACK);
+    //gfx->setFont(&NotoSansBold36);
+    gfx->setFont(&FreeSansBold10pt7b);
+    gfx->println("70");
+    gfx->setCursor(20,35);
+    gfx->setTextColor(BLACK);
+    gfx->setFont(&FreeMono8pt7b);
+    gfx->println("km/h");
     delay(2000); // 1 second
 }
