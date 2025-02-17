46fe26b7564f75c6d6dd6bc1c1e1c4c465fb80bd
Joost Soeterbroek <j.soeterbroek@dictu.nl>
2025-02-17 11:27:27 +0100

    add working

--- dashboard/dashboard.ino
+++ dashboard/dashboard.ino
@@ -7,6 +7,13 @@
 #include <TFT_eSPI.h>
 #include "Free_Fonts.h" // Include the header file attached to this sketch
 
+#define SCREEN_WIDTH 240
+#define SCREEN_HEIGHT 320
+#define TFT_GREY 0x5AEB
+
+TFT_eSPI tft = TFT_eSPI();
+
+// status flags
 bool SETUP_OK = false;
 bool ESP_SETUP_OK = false;
 bool ESP_DATA_RECVD_OK = false;
@@ -18,28 +25,6 @@ bool TEMP_OK = false;
 bool TEMP_DATA_RECVD_OK = false;
 bool BATT_OK = false;
 bool BATT_DATA_RECVD_OK = false;
-
-
-//       An abbreviation of the font name. Look at the list below to see
-//       the abbreviations used, for example:
-//
-//       tft.setFreeFont(FSSBO24)
-//
-//       Where the letters mean:
-//       F = Free font
-//       M = Mono
-//      SS = Sans Serif (double S to distinguish is form serif fonts)
-//       S = Serif
-//       B = Bold
-//       O = Oblique (letter O not zero)
-//       I = Italic
-//       # =  point size, either 9, 12, 18 or 24
-
-#define SCREEN_WIDTH 240
-#define SCREEN_HEIGHT 320
-#define TFT_GREY 0x5AEB
-
-TFT_eSPI tft = TFT_eSPI();
 
 //Structure example to send data
 //Must match the receiver structure
@@ -64,11 +49,6 @@ typedef struct struct_message {
 // Create a struct_message to hold incoming data
 struct_message myData;
 
-// ESPNow checks
-volatile bool data_ready = false;
-bool startup_ready = false;
-bool startup_complete = false;
-
 //callback function that will be executed when data is received
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   memcpy(&myData, incomingData, sizeof(myData));
@@ -85,6 +65,8 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   Serial.println(myData.gps_lng);
   Serial.println("-------");
   ESP_DATA_RECVD_OK = true;
+  // TODO: check actual GPS data
+  GPS_DATA_RECVD_OK = true;
 }
 
 void setup() {
@@ -105,8 +87,8 @@ void setup() {
     return;
   }
   ESP_SETUP_OK = true;
-  // Once ESPNow is successfully Init, we will register for recv CB to
-  // get recv packer info
+  // Once ESPNow is successfully Init, we will register for recv CB
+  // to get recv packer info
   esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
 
   // setup 'splash' screen
@@ -124,12 +106,6 @@ void setup() {
 
 void loop() {
 
-  // if (startup_ready && !startup_complete) {
-  //     delay(120);
-  //     start_splash();
-  //     startup_ready = false;
-  //  }
-
   if (ESP_DATA_RECVD_OK) {
     draw();
   }
@@ -145,20 +121,43 @@ void draw() {
   tft.setTextColor(TFT_GREY);
 
   /*****************************************************************************
+   * 'led' warning lights
+   * 
+   *  gps -> orange : indoors, (test) data received ok
+   *  gps -> green  : outdoors, data received ok
+   *  gps -> grey   : data not received
+   ****************************************************************************/
+  tft.drawString("gps", 120, 20, GFXFF);  
+  if (GPS_INDOORS_TEST) {
+      if (GPS_DATA_RECVD_OK) {
+          tft.fillSmoothCircle(110, 26, 4, TFT_ORANGE);
+      } else {
+          tft.fillSmoothCircle(110, 26, 4, TFT_GREY);
+      }
+  } else {
+      if (GPS_DATA_RECVD_OK) {
+          tft.fillSmoothCircle(110, 26, 4, TFT_GREEN);
+      } else {
+          tft.fillSmoothCircle(110, 26, 4, TFT_GREY);
+      }
+  }
+
+  /*****************************************************************************
    * GPS Time 
    ****************************************************************************/
   x = 170; y = 4; w = 140; h = 64;
   tft.drawRoundRect(x, y, w, h, 10, TFT_GREY);
   tft.setFreeFont(FSSBO18);
-  // make string "HH:MM"
-  // TODO: 24-hours format
-  int fu;
-  char buffer[50];
-  fu = sprintf(buffer, "%.2i:%.2i",
-    myData.gps_time_hour,
-    myData.gps_time_minute);
-  String gps_time(buffer);
-  tft.drawString(gps_time, x+6, y+8);
+  if (GPS_DATA_RECVD_OK) {
+    // make string "HH:MM"
+    // TODO: 24-hours format
+    char buffer[50];
+    sprintf(buffer, "%.2i:%.2i",
+      myData.gps_time_hour,
+      myData.gps_time_minute);
+    String gps_time(buffer);
+    tft.drawString(gps_time, x+6, y+8);
+  }
   tft.setFreeFont(FF1);
   tft.drawString("GPS Time", x+10, y+42, GFXFF);
 
@@ -168,9 +167,26 @@ void draw() {
   x = 170; y = 70; w = 140; h = 70;
   tft.drawRoundRect(x, y, w, h, 10, TFT_GREY);
   tft.setFreeFont(FF1);
-  tft.drawNumber(myData.gps_lat, x+6, y+8);  
-  tft.drawNumber(myData.gps_lng, x+6, y+28);  
+  if (GPS_DATA_RECVD_OK) {
+    tft.drawNumber(myData.gps_lat, x+6, y+8);  
+    tft.drawNumber(myData.gps_lng, x+6, y+28);  
+  }
   tft.drawString("GPS lat/lon", x+6, y+50, GFXFF);
+
+  /*****************************************************************************
+   * GPS speed 
+   ****************************************************************************/  
+  //  double gps_speed_kmph
+  x = 170; y = 142; w = 140; h = 70;
+  tft.drawRoundRect(x, y, w, h, 10, TFT_GREY);
+  if (GPS_DATA_RECVD_OK) {
+    tft.setFreeFont(FSSBO18);
+    tft.drawNumber(myData.gps_speed_kmph, x+6, y+8);  
+  }
+  tft.setFreeFont(FF1);
+  tft.drawString("Ground Speed (K/h)", x+6, y+50, GFXFF);
+
+  //  double gps_altitude_meters
 
   /*****************************************************************************
    * Temp 
@@ -191,12 +207,6 @@ void draw() {
   tft.drawFloat(myData.hum, 0, x+5, y+8);  
   tft.setFreeFont(FF1);
   tft.drawString("Humid.", x+8, y+42, GFXFF);  
-
-  /*****************************************************************************
-   * 'led' warning lights
-   ****************************************************************************/
-  tft.fillSmoothCircle(300, 14, 4, TFT_RED);
-
 
   delay(1000);
 }
